68b9876e6b7734d70f7cd6d7b39c54f54b881723
ajcoder42 <84202466+ajcoder42@users.noreply.github.com>
2023-09-10 02:33:35 +0530

    first commit

---
+++ src/algorithms/astar.js
@@ -0,0 +1,85 @@
+// Implementing A* algorithm.
+// A* is a weighted algorithm, weights are non-negative in this algorithm.
+
+// Performs A* algorithm; returns *all* nodes in the order
+// in which they were visited. Also makes nodes point back to their
+// previous node, effectively allowing us to compute the shortest path
+// by backtracking from the finish node.
+
+export function astar(grid, startNode, endNode) {
+  const visitedNodesInOrder = []; // To animate visited nodes in order to animate the process of searching.
+  startNode.distance = 0;
+  const unvisitedNodes = getAllNodes(grid);
+
+  while (unvisitedNodes.length) {
+    sortNodesByDistance(unvisitedNodes);
+    const closestNode = unvisitedNodes.shift();
+
+    // If we encounter a wall, we skip it.
+    if (closestNode.isWall) continue; // Here isWall is the property of the node.
+
+    // If the closest node is at a distance of infinity,
+    // we must be trapped and should therefore stop.
+    if (closestNode.distance === Infinity) return visitedNodesInOrder;
+    closestNode.isVisited = true;
+
+    visitedNodesInOrder.push(closestNode);
+
+    // Checking for the success condition.
+    if (closestNode === endNode) return visitedNodesInOrder;
+
+    updateUnvisitedNeighbors(closestNode, endNode, grid);
+  }
+}
+
+// Sorts the unvisitedNodes every time it is called.
+function sortNodesByDistance(unvisitedNodes) {
+  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+}
+
+// Sets the neighbour nodes distance and also the previous node property.
+function updateUnvisitedNeighbors(node, endNode, grid) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+  for (const neighbor of unvisitedNeighbors) {
+    const tentativeDistance = node.distance + neighbor.weight + 1;
+
+    if (tentativeDistance < neighbor.distance) {
+      neighbor.distance = tentativeDistance;
+      neighbor.previousNode = node; // With this property we can backtrack and find the shortest path between the start and end node.
+    }
+  }
+}
+
+function getUnvisitedNeighbors(node, grid) {
+  const neighbors = [];
+  const { col, row } = node; //col and row are properties of the node.
+
+  if (row > 0) neighbors.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+  if (col > 0) neighbors.push(grid[row][col - 1]);
+  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+
+  return neighbors.filter((neighbor) => !neighbor.isVisited); // filtering the visited node
+}
+
+function getAllNodes(grid) {
+  const nodes = [];
+  for (const row of grid) {
+    for (const node of row) {
+      nodes.push(node);
+    }
+  }
+  return nodes;
+}
+
+// Backtracks from the finishNode to find the shortest path.
+// Only works when called *after* the astar method above.
+export function getNodesInShortestPathOrderAstar(finishNode) {
+  const nodesInShortestPathOrder = [];
+  let currentNode = finishNode;
+  while (currentNode !== null) {
+    nodesInShortestPathOrder.unshift(currentNode);
+    currentNode = currentNode.previousNode;
+  }
+  return nodesInShortestPathOrder;
+}
